e6350fce9fa9b16f16ecf0ccc8f68a7bbe8bbb75
Hemant <hemant.phutela@gmail.com>
2018-09-23 23:47:43 -0400

    Initial Commit

---
+++ MAPD714_HemantPhutela_Assignment1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPD714_HemantPhutela_Assignment1
+//
+//  Created by Hemant Phutela on 23/09/2018.
+//  Copyright © 2018 Hemant Phutela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
