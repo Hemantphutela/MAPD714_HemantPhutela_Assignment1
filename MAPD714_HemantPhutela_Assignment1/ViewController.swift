fe5108c4e6101321e546ac8bb10550a9f8fd9202
Hemant <hemant.phutela@gmail.com>
2018-09-24 00:51:59 -0400

    Design

--- MAPD714_HemantPhutela_Assignment1/ViewController.swift
+++ MAPD714_HemantPhutela_Assignment1/ViewController.swift
@@ -9,7 +9,41 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var numberOnscreen:Double = 0;
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func numbers(_ sender: Any)
+    {
+        label.text = label.text! + String((sender as AnyObject).tag-1)
+        numberOnscreen = Double(label.text!)!
+    }
+    
+    @IBAction func buttons(_ sender: UIButton)
+    {
+        if label.text != "" && sender.tag != 11 && sender.tag != 16
+        {
+            if sender.tag == 12 //divide
+            {
+                
+            }
+            else if sender.tag == 13 //Multiply
+            {
+            
+            }
+            else if sender.tag == 14 //Minus
+            {
+                
+            }
+            else if sender.tag == 15 //Plus
+            {
+                
+            }
+            }
+        }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
